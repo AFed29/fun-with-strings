2f4b14fb9092a126c7dc5ac46cc3514a7f85bf0b
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2018-02-20 16:29:39 +0000

    Added methods for checking pallindromes and isograms

--- models/string_fun.rb
+++ models/string_fun.rb
@@ -1,11 +1,28 @@
 class StringFun
 
   def self.equal(string1, string2)
-    if string1 == string2
+    if string1.downcase == string2.downcase
       return "#{string1.capitalize} is the same as #{string2}"
-    else
-      return "#{string1.capitalize} is not the same as #{string2}"
     end
+    return "#{string1.capitalize} is not the same as #{string2}"
+  end
+
+  def self.pallindrome(string)
+    if string.downcase == string.downcase.reverse()
+      return "#{string.capitalize} is a pallindrome"
+    end
+    return "#{string.capitalize} is not a pallindrome"
+  end
+
+  def self.isogram(string)
+    previous_char = ""
+    string.downcase.each_char do |char|
+      if previous_char == char
+        return "#{string.capitalize} is not an isogram"
+      end
+      previous_char = char
+    end
+    return "#{string.capitalize} is an isogram"
   end
 
 end
--- specs/string_fun_spec.rb
+++ specs/string_fun_spec.rb
@@ -5,6 +5,7 @@ class TestStringFun < MiniTest::Test
   def setup
     @string1 = "hello"
     @string2 = "goodbye"
+    @string3 = "racecar"
   end
 
   def test_if_equal__equal()
@@ -12,9 +13,29 @@ class TestStringFun < MiniTest::Test
     assert_equal("Hello is the same as hello", result)
   end
 
-  def test_if_equal__not_equal
+  def test_if_equal__not_equal()
     result = StringFun.equal(@string1, @string2)
     assert_equal("Hello is not the same as goodbye", result)
+  end
+
+  def test_if_pallindrome__is_pallindrome()
+    result = StringFun.pallindrome(@string3)
+    assert_equal("Racecar is a pallindrome", result)
+  end
+
+  def test_if_pallindrome__is_not_pallindrome()
+    result = StringFun.pallindrome(@string1)
+    assert_equal("Hello is not a pallindrome", result)
+  end
+
+  def test_if_isogram__is_isogram()
+    result = StringFun.isogram(@string3)
+    assert_equal("Racecar is an isogram", result)
+  end
+
+  def test_if_isogram__is_not_isogram
+    result = StringFun.isogram(@string1)
+    assert_equal("Hello is not an isogram", result)
   end
 
 end
